f5fb4a842b7b505b5c4e9456746af3f795d61dc0
KEI <68884990+kei1992@users.noreply.github.com>
2021-01-27 17:21:48 +0900

    multiple-state-branch

--- src/App.js
+++ src/App.js
@@ -1,47 +1,30 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-
-  const increment2 = () => setCount((previousCount) => previousCount + 1);
-  const decrement2 = () => setCount((previousCount) => previousCount - 1);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
   const reset = () => {
-    setCount(0);
-  };
-  const double = () => {
-    setCount(count * 2);
-  };
-
-  const devidedThree = () => {
-    if (count % 3 === 0) {
-      return setCount(count / 3);
-    }else{
-      return count
-    }
+    setPrice(props.price);
+    setName(props.name);
   };
 
   return (
-    <React.Fragment>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>+1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>x2</button>
-        <button onClick={devidedThree}>3の倍数の時だけ3で割る</button>
-      </div>
-    </React.Fragment>
+    <>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+    </>
   );
+};
+
+App.defaultProps = {
+  name: "",
+  price: 1000,
 };
 
 export default App;

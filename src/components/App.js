b967ad06c753d891ade00a2c223852465eba077b
KEI <68884990+kei1992@users.noreply.github.com>
2021-02-06 12:56:33 +0900

    implete-all-events-button

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,16 @@ const App = () => {
     setBody("");
   };
 
+  const deleteAllEvents = (e) => {
+    e.preventDefault();
+    const result = window.confirm(
+      "全てのイベントを本当に削除しても良いですか？"
+    );
+    if (result) dispatch({ type: "DELETE_ALL_EVENTS" });
+  };
+
+  const unCreatable = title === "" || body === "";
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -43,10 +53,20 @@ const App = () => {
             onChange={(e) => setBody(e.target.value)}
           />
         </div>
-        <button className="btn btn-primary" onClick={addEvent}>
+        <button
+          className="btn btn-primary"
+          onClick={addEvent}
+          disabled={unCreatable}
+        >
           イベントを作成する
         </button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllEvents}
+          disabled={state.length === 0}
+        >
+          全てのイベントを削除する
+        </button>
       </form>
       <h4>イベント一覧</h4>
       <table className="table table-hover">
@@ -60,7 +80,7 @@ const App = () => {
         </thead>
         <tbody>
           {state.map((event, index) => {
-            return <Event key={index} event={event} />;
+            return <Event key={index} event={event} dispatch={dispatch} />;
           })}
         </tbody>
       </table>
